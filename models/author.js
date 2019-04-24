fbe5872e2da8ac1e902c21a943e85750648ac10c
JonatanNissi <jonu.nissi@gmail.com>
2019-04-24 18:45:16 +0300

    wuu

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment'); // For date handling.
 
 var Schema = mongoose.Schema;
 
@@ -8,6 +9,7 @@ var AuthorSchema = new Schema(
     family_name: {type: String, required: true, max: 100},
     date_of_birth: {type: Date},
     date_of_death: {type: Date},
+    nationality: {type: String,}
   }
 );
 
@@ -22,7 +24,27 @@ AuthorSchema
 AuthorSchema
 .virtual('lifespan')
 .get(function () {
-  return (this.date_of_birth).format('MMMM Do, YYYY') - (this.date_of_death).format('MMMM Do, YYYY').toString();
+  var lifetime_string='';
+  if (this.date_of_birth) {
+      lifetime_string=moment(this.date_of_birth).format('MMMM Do, YYYY');
+      }
+  lifetime_string+=' - ';
+  if (this.date_of_death) {
+      lifetime_string+=moment(this.date_of_death).format('MMMM Do, YYYY');
+      }
+  return lifetime_string
+});
+
+AuthorSchema
+.virtual('date_of_birth_yyyy_mm_dd')
+.get(function () {
+  return moment(this.date_of_birth).format('YYYY-MM-DD');
+});
+
+AuthorSchema
+.virtual('date_of_death_yyyy_mm_dd')
+.get(function () {
+  return moment(this.date_of_death).format('YYYY-MM-DD');
 });
 
 // Virtual for author's URL
@@ -30,6 +52,16 @@ AuthorSchema
 .virtual('url')
 .get(function () {
   return '/catalog/author/' + this._id;
+});
+
+// Virtual for author's nationality
+AuthorSchema
+.virtual('nationality')
+.get(function () {
+  return this.nationality;})
+  .set(function(nationality) {
+  var one = nationality.one('');
+  this.nationality.here = nationality.substr(0, nationality.indefOf('  '));
 });
 
 //Export model
